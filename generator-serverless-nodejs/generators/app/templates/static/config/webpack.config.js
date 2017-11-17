c14d4c0032d6ca714998e9a1fc8c3c997455b326
Stephen Hogg <steve@h4.co.nz>
2017-11-17 14:23:07 +1300

    fix: update webpack.config.js to use correct directory

--- generator-serverless-nodejs/generators/app/templates/static/config/webpack.config.js
+++ generator-serverless-nodejs/generators/app/templates/static/config/webpack.config.js
@@ -3,14 +3,17 @@ const path = require('path')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const plugins = [
-  // // fix: webpack require('crypto') TypeError: require is not a function
-  // //      lambdas give a "module initialization error: TypeError" error otherwise
+  // fix: webpack require('crypto') TypeError: require is not a function
+  //      lambdas give a "module initialization error: TypeError" error otherwise
   // new webpack.DefinePlugin({ 'global.GENTLY': false })
 ]
 
 if (process.env.NODE_ENV !== 'test') {
   plugins.push(new UglifyJSPlugin())
 }
+
+// main directory is the current one's parent
+const dir = path.join(__dirname, '/..')
 
 module.exports = {
   entry: './src/handler.js',
@@ -25,14 +28,14 @@ module.exports = {
       {
         test: /\.js$/,
         loaders: [ 'babel-loader' ],
-        include: __dirname,
+        include: dir,
         exclude: /node_modules/
       }
     ]
   },
   output: {
     libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
+    path: path.join(dir, '.webpack'),
     filename: 'src/handler.js'
   }
 }
